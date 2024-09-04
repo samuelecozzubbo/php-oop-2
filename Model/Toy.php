8d4c2962b7005646d6ea97a29d00b89a0aa03fe1
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-04 16:18:30 +0200

    Build: created toy class

---
+++ Model/Toy.php
@@ -0,0 +1,13 @@
+<?php
+
+class Toy extends Product
+{
+    public $material;
+    public $size;
+    public function __construct(string $_title, float $_price, string $_image, Category $_category, string $_material, int $_size)
+    {
+        parent::__construct($_title, $_price, $_image, $_category, "Toy");
+        $this->material = $_material;
+        $this->size = $_size;
+    }
+}
